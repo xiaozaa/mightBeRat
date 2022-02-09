cdef409d26d24aefa37cf4f70d81039f57786354
xiaozaa <35381760+xiaozaa@users.noreply.github.com>
2022-02-09 10:01:05 -0800

    Update rat.py

--- rat.py
+++ rat.py
@@ -17,26 +17,28 @@ for index in range(100):
       "Accept": "application/json",
       "X-API-KEY": openseaAPI
   }
+  try: 
+    response = requests.request("GET", url, headers=headers)
 
-  response = requests.request("GET", url, headers=headers)
+    sales = json.loads(response.text)
 
-  sales = json.loads(response.text)
+    if('asset_events' in sales):
+      if(sales['asset_events']):
+        for trans in sales["asset_events"]:
+          buyer = trans["winner_account"]
+          price = int(trans["total_price"])/1000000000000000000
+          #print(buyer["address"])
+          eth_url = "https://api.etherscan.io/api?module=account&action=txlist&address="+buyer["address"]+"&startblock=0&endblock=99999999&offset=0&sort=asc&apikey="+str(etherscanAPI)
 
-  if('asset_events' in sales):
-    if(sales['asset_events']):
-      for trans in sales["asset_events"]:
-        buyer = trans["winner_account"]
-        price = int(trans["total_price"])/1000000000000000000
-        #print(buyer["address"])
-        eth_url = "https://api.etherscan.io/api?module=account&action=txlist&address="+buyer["address"]+"&startblock=0&endblock=99999999&offset=0&sort=asc&apikey="+str(etherscanAPI)
+          eth_res = requests.request("GET", eth_url)
+          txn = json.loads(eth_res.text)
+          #print(len(txn["result"]))
 
-        eth_res = requests.request("GET", eth_url)
-        txn = json.loads(eth_res.text)
-        #print(len(txn["result"]))
-
-        if('result' in txn):
-          if not (txn['result'] is None):
-            if(len(txn["result"]) < 6):
-              print(buyer["address"],' ', price,flush=True)
-
+          if('result' in txn):
+            if not (txn['result'] is None):
+              if(len(txn["result"]) < 6):
+                print(buyer["address"],' ', price,flush=True)
+  except:
+    print("This request is bad")
+    
   time.sleep(2)
