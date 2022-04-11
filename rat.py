3ba9a275ca88cd204e7d9de737e4a757be3dc563
xiaozaa <35381760+xiaozaa@users.noreply.github.com>
2022-04-10 19:51:43 -0700

    Update rat.py

--- rat.py
+++ rat.py
@@ -11,7 +11,13 @@ for index in range(100):
   limit = 200
   offset = index * limit
 
-  url = "https://api.opensea.io/api/v1/events?asset_contract_address="+str(contract)+"&event_type=successful&only_opensea=true&offset="+str(offset)+"&limit="+str(limit)+"&occurred_before="+str(timeNow)
+  
+  url = "https://api.opensea.io/api/v1/events?asset_contract_address="+str(contract)+"&event_type=successful"
+
+  if(cursor):
+    url = url + "&cursor="+cursor
+
+  print(url);
 
   headers = {
       "Accept": "application/json",
@@ -22,23 +28,27 @@ for index in range(100):
 
     sales = json.loads(response.text)
 
+
+    if("next" in sales):
+      cursor = sales['next']
+
     if('asset_events' in sales):
       if(sales['asset_events']):
         for trans in sales["asset_events"]:
           buyer = trans["winner_account"]
           price = int(trans["total_price"])/1000000000000000000
-          #print(buyer["address"])
+          # print(buyer["address"])
           eth_url = "https://api.etherscan.io/api?module=account&action=txlist&address="+buyer["address"]+"&startblock=0&endblock=99999999&offset=0&sort=asc&apikey="+str(etherscanAPI)
 
           eth_res = requests.request("GET", eth_url)
           txn = json.loads(eth_res.text)
-          #print(len(txn["result"]))
-
+          # print(len(txn["result"]))
+          cnt += 1;
+          print(cnt);
           if('result' in txn):
             if not (txn['result'] is None):
               if(len(txn["result"]) < 6):
                 print(buyer["address"],' ', price,flush=True)
-  except:
+  except Exception as ex:
     print("This request is bad")
-    
-  time.sleep(2)
+    print(ex)
